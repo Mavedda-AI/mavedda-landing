b5ba1d541a25b88349f0089c767edc3b387d1a32
hidirektor <hidirektor@gmail.com>
2026-07-08 00:29:43 +0300

    General UI Update
    Signed-off-by: hidirektor <hidirektor@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type {Metadata} from "next";
 import {Inter, Outfit} from "next/font/google";
 import "./globals.css";
-import "./webflow.css";
 import {AppProviders} from "@/providers/AppProviders";
-import Script from "next/script";
+import {LanguageProvider} from "@/providers/LanguageProvider";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -35,15 +34,11 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${outfit.variable} antialiased`}
       >
-        <AppProviders>
-          {children}
-        </AppProviders>
-        
-        <Script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66b34f2ad59081546d14c723" strategy="beforeInteractive" crossOrigin="anonymous" />
-        <Script src="https://cdn.prod.website-files.com/66b34f2ad59081546d14c723/js/webflow.schunk.f2efb3c5440a81cf.js" strategy="afterInteractive" crossOrigin="anonymous" />
-        <Script src="https://cdn.prod.website-files.com/66b34f2ad59081546d14c723/js/webflow.schunk.b8e2fe60f292f63f.js" strategy="afterInteractive" crossOrigin="anonymous" />
-        <Script src="https://cdn.prod.website-files.com/66b34f2ad59081546d14c723/js/webflow.schunk.a3706067fad6072f.js" strategy="afterInteractive" crossOrigin="anonymous" />
-        <Script src="https://cdn.prod.website-files.com/66b34f2ad59081546d14c723/js/webflow.72266a5d.39b916739de17552.js" strategy="afterInteractive" crossOrigin="anonymous" />
+        <LanguageProvider>
+          <AppProviders>
+            {children}
+          </AppProviders>
+        </LanguageProvider>
       </body>
     </html>
   );
